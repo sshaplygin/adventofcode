7547f757ea01d646b9ca2321fbb50113ae54e5d3
Saimon Shaplygin <semyuon@gmail.com>
2023-12-23 16:22:49 +0100

    add: day_7 part 2

--- 2023/day_7/main.py
+++ 2023/day_7/main.py
@@ -1,15 +1,16 @@
-from collections import Counter
 import heapq
+
+from collections import Counter
 
 def parse_input(is_test = False):
     text = []
     if is_test:
         text = [
-            '32T3K 765', # two [1, 1], 0 rank = 1
-            'T55J5 684', # three [2, 3], 1 rank = 4
-            'KK677 28',  # two  [1, 11] 2 rank = 3
-            'KTJJT 220', # two [1, 8] 3, rank = 2
-            'QQQJA 483', # three [2, 10] 4, rank = 5
+            '32T3K 765',
+            'T55J5 684',
+            'KK677 28',
+            'KTJJT 220',
+            'QQQJA 483',
         ]
     else:
         with open('input.txt', 'r') as file:
@@ -25,49 +26,67 @@ def parse_input(is_test = False):
     return [hands, bids]
 
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-cards_mapping = {value: index for index, value in enumerate(cards)}
+cards_with_joker = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
+
+cards_without_joker = {value: index for index, value in enumerate(cards)}
+cards_with_joker = {value: index for index, value in enumerate(cards_with_joker)}
+
 types_priority = ['high', 'one', 'two', 'three', 'full_house', 'four', 'five']
 
-def get_values(hand):
+def get_values(hand, with_joker = False):
     count_cards = Counter(hand)
     cards_arr = []
+
+    cards_mapping = cards_without_joker
+    if with_joker:
+        cards_mapping = cards_with_joker
+    
     for card in count_cards:
         cards_arr.append((count_cards[card], cards_mapping[card]))
-    sorted_list = sorted(cards_arr, key = lambda x: (-x[0], -x[1]))
+    hand_priority = sorted(cards_arr, key = lambda x: (x[0], x[1]), reverse=True)
     
-    t = len(types_priority)-1
-    if sorted_list[0][0] == 1:
-        t = 0
-    if sorted_list[0][0] == 2:
-        if len(sorted_list) > 3:
-            t = 1
-        else:
-            t = 2
-    if sorted_list[0][0] == 3:
-        if len(sorted_list) == 2:
-            t = 4
-        else:
-            t = 3
-    if sorted_list[0][0] == 4:
-        t = 5
-    res = [t]
+    res = [get_hand_type(count_cards, hand_priority, with_joker)]
     for card in hand:
         res.append(cards_mapping[card])
     return res
 
-def count_total_winnings(data):
+def get_hand_type(count_cards: Counter, hand_priority, with_joker=False):
+    t = len(types_priority)-1
+    if hand_priority[0][0] == 1:
+        t = 0
+    if hand_priority[0][0] == 2:
+        if len(hand_priority) > 3:
+            t = 1
+        else:
+            t = 2
+    if hand_priority[0][0] == 3:
+        if len(hand_priority) == 2:
+            t = 4
+        else:
+            t = 3
+    if hand_priority[0][0] == 4:
+        t = 5
+
+    if with_joker and hand_priority[0][1] != 0:
+        t += count_cards.get('J', 0)
+
+    return t
+
+def count_total_winnings(data, with_joker = False):
     [hands, bids] = data
     h = []
     for idx, hand in enumerate(hands):
-        heapq.heappush(h, (get_values(hand), idx))
+        heapq.heappush(h, (get_values(hand, with_joker), idx))
     
     total = 0
     for i in range(len(hands)):
-        [_, idx] = heapq.heappop(h)
-        print(f'hand={hands[idx]} bid={bids[idx]} rank={i+1}')
+        [vals, idx] = heapq.heappop(h)
         total += (i + 1) * bids[idx]
+        # debug
+        print(f'hand={hands[idx]} type={vals[0]} bid={bids[idx]} rank={i+1}')
     return total
 
 if __name__ == '__main__':
-    data = parse_input()
+    data = parse_input(True)
     print(count_total_winnings(data))
+    print(count_total_winnings(data, True))
